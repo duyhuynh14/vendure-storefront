27fed2c7d404e84d5443dd34c6a9ef79b0870bbf
duyhuynh14 <tommyhuynh14@gmail.com>
2020-12-08 21:29:46 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     apiHost: 'https://duy-nginx.herokuapp.com',
-    apiPort: 443,
+    apiPort: parseInt(<string>process.env.PORT, 10) || 443,
     shopApiPath: 'shop-api',
     baseHref: '/',
     tokenMethod: 'cookie',
